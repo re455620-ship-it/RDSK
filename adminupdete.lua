3bc879d562f181c224d1bb30aa11de2ea4e1f4ce
re455620-ship-it <re455620@gmail.com>
2026-01-09 18:54:55 -0300

    Administrador 2.1 atualizado!
    Esse ADM não e top...

--- adminupdete.lua
+++ adminupdete.lua
@@ -542,4 +542,13 @@ end
 btn(tabs.Jogos, "Escape da Lava (Lendas Brabas)", 10).MouseButton1Click:Connect(function()
     executarEscapeLava()
     print("Script Lendas Brabas executado!")
+end) -------------------- JOGOS 🎮 (CONTINUAÇÃO) --------------------
+
+-- Botão do Gg Hub (Abaixo do Escape da Lava)
+-- Usei y = 50 para ele ficar logo abaixo do primeiro botão
+btn(tabs.Jogos, "Gg Hub (Undertale Zombies)", 50).MouseButton1Click:Connect(function()
+    -- Carrega o script que você salvou no GitHub
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/re455620-ship-it/RDSK/main/Admin.lua"))()
+    print("Gg Hub ativado com sucesso!")
 end)
+
